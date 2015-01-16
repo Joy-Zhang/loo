db43e613a8a8dbf5e88cc006773e0be93cea28b8
Joy Zhang <twoleft.zhang@outlook.com>
2015-01-16 17:27:47 +0800

    Add a simple orm framework. Enhance the error handling.

--- loo/app.lua
+++ loo/app.lua
@@ -3,17 +3,36 @@ local M = {}
 _G[modname] = M
 
 M.router = require('loo.router')
+M.model = require('loo.model')
 
-M.run = function()
+local _run = function() 
     local controller_module, call, args = M.router.route(ngx.req.get_method(), ngx.var.uri)
-    if call then
-        local controller = require(controller_module)
-        controller[call](unpack(args))
-    else
-        ngx.exit(ngx.HTTP_NOT_FOUND)
+    if call == nil then
+        return false, 'routing uri "'..ngx.var.uri..'" failed'
+    end 
+    local ok, controller = pcall(require, controller_module)
+    if not ok then
+        return false, 'error when loading controller "'..controller_module..'"\n'..controller
+    end
+    if type(controller[call]) ~= 'function' then
+        return false, 'controller function "'..call..'" not found'
+    end
+    M.model._ctor()
+    ok, err = pcall(controller[call], unpack(args))
+    M.model._finalize()
+    if not ok then
+        return false, err
+    end
+    return true
 end
 
-
+M.run = function()
+    local ok, err = _run()
+    if not ok then
+        ngx.header['Content-Type'] = 'text/plain'
+        ngx.status = ngx.HTTP_NOT_FOUND
+        ngx.print(err)
+    end
 end
 
 return M
---
+++ loo/model.lua
@@ -0,0 +1,118 @@
+local modname = ...
+local M = {}
+_G[modname] = M
+
+local mysql = require('resty.mysql')
+
+M.db = 'mysql'
+M.host = '127.0.0.1'
+M.port = '3306'
+M.database = ''
+M.user = 'root'
+M.password = ''
+
+local db = nil
+local information = {}
+
+
+
+M.get = function(model_name)
+    if M.db == nil then
+        return nil, 'db not set'
+    end
+    local model = {
+        table = model_name
+    } 
+
+    if information[model_name] == nil then
+        local columns, err = M.query('SELECT * FROM INFORMATION_SCHEMA.COLUMNS\
+            WHERE TABLE_SCHEMA = \''..M.database..'\' AND TABLE_NAME = \''..model_name..'\'')
+        for i, column in ipairs(columns) do
+            if column['COLUMN_KEY'] == 'PRI' then
+                model.key = column['COLUMN_NAME']
+            end
+        end
+        model.columns = columns
+        information[model_name] = model
+    end
+    
+    model.create = function(self, row) 
+        local columns = ''
+        local values = ''
+        local i = 0
+        for name, value in pairs(row) do
+            if i == 0 then
+                columns = columns..'('
+                values = values..'('
+            else
+                columns = columns..','
+                values = values..','
+            end
+            i = i + 1            
+            if type(value) == 'string' then
+                columns = columns..name
+                values = values..string.format('\'%s\'', value)
+            elseif type(value) == 'number' then
+                columns = columns..name
+                values = values..string.format('%d', value)
+            end
+        end
+        
+        local sql = 'INERT INTO '..self.table..columns..') VALUES '..values..')'
+        return sql
+    end
+
+
+    return information[model_name]
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+M.query = function(query)
+    if M.db == nil then
+        return nil, 'db not set'
+    end
+    return db:query(query)
+end
+
+M._ctor = function()
+    if M.db == nil then
+        return
+    end
+    db = mysql:new()
+    local ok, err = db:connect({
+        host = M.host,
+        port = M.port,
+        database = M.database,
+        user = M.user,
+        password = M.password
+    })
+    if err then
+        ngx.header['Content-Type'] = 'text/plain'
+        ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
+        ngx.print(err)
+        ngx.exit(ngx.OK)
+    end
+end
+
+M._finalize = function() 
+    if M.db == nil then
+        return
+    end
+    db:close()
+end
+
+
+return M
+
