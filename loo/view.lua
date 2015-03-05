020f2bb417ad5f73bd52d5a4ae857fcb6a4351d9
Joy Zhang <twoleft.zhang@outlook.com>
2015-03-05 17:09:27 +0800

    support table value arguement in template

--- loo/view.lua
+++ loo/view.lua
@@ -13,8 +13,18 @@ __M.__call = function(M, view_name)
     local view = {};
     view.template = template;
     view.render = function(self, args)
-        local output = ngx.re.gsub(template, '\\{#(.+?)\\}', function(m)
-                return args[m[1]]
+        local output = ngx.re.gsub(template, '\\{#(.+?)\\}', function(path)
+                local value = args
+                ngx.re.gsub(path[1], '([^\\.]+)', function(part)
+                        value = value[part[1]]
+                        return part[0]
+                    end
+                )
+                if type(value) ~= 'string' then
+                    return type(value)..'(string only)'
+                else
+                    return value
+                end
             end
         )
         ngx.print(output)
